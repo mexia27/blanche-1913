b0f663adff53597fc815336a67b363ca01af5aed
Stephen.agyapong <Stephen.agyapong@fda.hhs.gov>
2020-01-23 14:16:40 -0500

    joker comnits

--- new.sh
+++ new.sh
@@ -1,2 +1,4 @@
 1)I love this procedure of doing things in a very smart and peaceful manner
 2) I love the way things get done in this new environment,i feel really great about it.
+3)this should do the trick in all we have learnt so far
+4) ommiting this wonderful request will help us alot for what we have done.
